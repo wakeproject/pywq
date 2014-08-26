f23a5b80444bb587d33b5897506e7089db97171f
Mingli Yuan <mingli.yuan@gmail.com>
2014-08-26 17:53:20 +0800

    add skeleton of the package

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='pywq',
+    version='0.1.0',
+    author='Mingli Yuan',
+    author_email='mingli.yuan@gmail.com',
+    packages=['wq'],
+    url='git+https://github.com/Wahlque/pywq.git',
+    license='LICENSE',
+    description='The python library for Wahlque project',
+    long_description=open('README.txt').read(),
+    install_requires=[
+    ],
+)
